4c2735ac2045bd2f4910a6cbce7b4432273fd697
MaligynK <skull_mk@mail.ru>
2017-06-12 00:36:16 +0300

    События для Book

--- index_script.js
+++ index_script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let HTML_ID = 'mkj_book_elem_';
+let current_index = 1;
 
 class Book {
 
@@ -8,10 +9,33 @@ class Book {
         return 'mkj_book_elem_' + this._id;
     }
 
+    edit(){
+
+        console.log('!!!!!!!!!!!!')
+    }
+
+    remove(){
+        let html_block = this._html_elem.find('.mkj-block');
+        html_block.find('.mkj-edit').off('click');
+        html_block.find('.mkj-remove').off('click');
+        html_block.off('click');
+        html_block.off('mouseleave');
+        html_block.off('mouseenter');
+
+        this._html_elem.remove();
+
+        console.log('!!!!!!!RM')
+    }
+
+    choice(){
+        console.log('!!!!!!CH')
+
+    }
+
     constructor(id, name, author, date, pages) {
         this._id = id;
 
-        let html_elem = $(
+        this._html_elem = $(
             '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 mkj-book-elem">' +
                 '<div class="mkj-block">' +
                     '<p class="mkj-book-title"></p>' +
@@ -27,22 +51,30 @@ class Book {
             '</div>'
         );
 
-        this._html_elem = html_elem;
-        html_elem.attr('id', this.html_id(id));
-        $('#mkj_book_container').append(html_elem);
+        this._html_elem.attr('id', this.html_id(id));
+        $('#mkj_book_container').append(this._html_elem);
 
 
-        // при помощи html_elem производим замыкание функции
-        // можно конечно заменить на $(event.target), но зачем
-        html_elem.mouseenter(function(event){
-            console.log('1', event)
-            html_elem.find('.mkj-edit').show();
-            html_elem.find('.mkj-remove').show();
+
+        let html_block = this._html_elem.find('.mkj-block');
+        html_block.find('.mkj-edit').on('click', ()=>{
+            this.edit();
+            return false;
         });
-        html_elem.mouseleave(function(event){
-            console.log('0', event)
-            html_elem.find('.mkj-edit').hide();
-            html_elem.find('.mkj-remove').hide();
+        html_block.find('.mkj-remove').on('click', ()=>{
+            this.remove();
+            return false;
+        });
+        html_block.on('click', this.choice);
+        // при помощи html_block производим замыкание функции
+        // можно конечно заменить на $(event.target), но зачем
+        html_block.mouseenter(function(event){
+            html_block.find('.mkj-edit').show();
+            html_block.find('.mkj-remove').show();
+        });
+        html_block.mouseleave(function(event){
+            html_block.find('.mkj-edit').hide();
+            html_block.find('.mkj-remove').hide();
         });
 
         this.set_data(name, author, date, pages);
@@ -58,14 +90,16 @@ class Book {
         this._html_elem.find('.mkj-book-date').text(date);
     }
 
-
 }
 
 
 let save_book = function(){
-    let t = new Book(1, '!!!!!', 'rtrth', 2001, 105);
+    current_index++;
+    let t = new Book(current_index, '!!!!!', 'rtrth', 2001, 105);
 
 };
 
-
-console.log('!!!!!!!!!!!');
+$(document).ready(function () {
+    console.log('!!!!!!!!!')
+    let t = new Book(current_index, '!!!!!', 'rtrth', 2001, 105);
+});
