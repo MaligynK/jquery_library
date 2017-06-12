469889f89de3326b7f8d1aec61d22591028e7815
MaligynK <skull_mk@mail.ru>
2017-06-12 23:46:36 +0300

    Комментарии и валидация

--- index_script.js
+++ index_script.js
@@ -1,23 +1,28 @@
 'use strict';
 
-let HTML_ID = 'mkj_book_elem_';
+// для генерации идентификаторов
 let current_index = 1;
+// книга выбранная для редактирования
 let current_book;
+// список объектов Book
 let books = [];
+// список данных для сохранения в localStorage
 let books_data = [];
 
 
+// Обеъкт книги с сылкой на DOM-элемент
 class Book {
 
     html_id(){
+        // id DOM-элемента
         return 'mkj_book_elem_' + this._id;
     }
-
     get id(){
+        // id объекта
         return this._id;
     }
-
     get json_data(){
+        // информация, которую сохраняем в localStorage
         return {
             id: this._id,
             name: this._name,
@@ -26,22 +31,27 @@ class Book {
             date: this._date
         }
     }
-
     edit(){
+        // редактирование книги
+        // делаем ее текущей
         current_book = this;
+        // вставляем данные в форму редактирования
         $('#mkj_book_name').val(this._name);
         $('#mkj_book_author').val(this._author);
         $('#mkj_book_pages').val(this._pages);
         $('#mkj_book_date').val(this._date);
     }
-
     remove(){
+        // удаление книги
         let html_block = this._html_elem.find('.mkj-block');
+        // отписываемся от всего (не уверен в необходимости)
         html_block.find('.mkj-edit').off('click');
         html_block.find('.mkj-remove').off('click');
         html_block.off('mouseleave');
         html_block.off('mouseenter');
+        // удаление DOM-элемента
         this._html_elem.remove();
+        // удаление из сохраняемой информации
         for(let i=0; i<books_data.length; i++){
             if(this._id == books_data[i].id){
                 books_data.splice(i, 1);
@@ -49,6 +59,7 @@ class Book {
                 break;
             }
         }
+        // удаление из списка книг
         for(let i=0; i<books.length; i++){
             if(this._id == books[i].id){
                 books.splice(i, 1);
@@ -56,14 +67,15 @@ class Book {
             }
         }
         if(this == current_book){
-            console.log('!!!!!!cur_del')
+            // книга была на редактировании, удаляем
             current_book = undefined;
+            //TODO: нужно ли очистить форму при этом?
         }
     }
 
     constructor(id, name, author, date, pages) {
         this._id = id;
-
+        // создание DOM-элемента
         this._html_elem = $(
             '<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 mkj-book-elem">' +
                 '<div class="mkj-block">' +
@@ -79,11 +91,8 @@ class Book {
                 '</div>' +
             '</div>'
         );
-
         this._html_elem.attr('id', this.html_id(id));
         $('#mkj_book_container').append(this._html_elem);
-
-
 
         let html_block = this._html_elem.find('.mkj-block');
         html_block.find('.mkj-edit').on('click', ()=>{
@@ -95,7 +104,7 @@ class Book {
             return false;
         });
         // при помощи html_block производим замыкание функции
-        // можно конечно заменить на $(event.target), но зачем
+        // можно, конечно, заменить на $(event.target), но не вижу смысла
         html_block.mouseenter(function(event){
             html_block.find('.mkj-edit').show();
             html_block.find('.mkj-remove').show();
@@ -105,14 +114,17 @@ class Book {
             html_block.find('.mkj-remove').hide();
         });
 
+        // присваиваем значения
         this.set_data(name, author, date, pages);
     }
 
     set_data(name, author, date, pages){
+        // присваиваем значения
         this._name = name;
         this._author = author;
         this._date = date;
         this._pages = pages;
+        // подставляем в DOM-элемент
         this._html_elem.find('.mkj-book-title').text('«' + name + '»');
         this._html_elem.find('.mkj-book-author').text(author);
         this._html_elem.find('.mkj-book-date').text(date);
@@ -120,15 +132,61 @@ class Book {
 
 }
 
+let check_book_eroors = function () {
+    // проверяем данные в редактируемой книге на ошибки
+    // возвращает true, если ошибки есть
+    let current_date = new Date();
+    let error = false;
+    let error_name = $('.mkj-view-name .text-danger');
+    let error_author = $('.mkj-view-author .text-danger');
+    let error_pages = $('.mkj-view-pages .text-danger');
+    let error_date = $('.mkj-view-date .text-danger');
+    if(name.length < 3){
+        // имя должно быть хотя бы 3 символа
+        error = true;
+        error_name.show();
+    }else{
+        error_name.hide();
+    }
+    if(author.length < 4){
+        // имя автора хотя бы 4 символа
+        error = true;
+        error_author.show();
+    }else{
+        error_author.hide();
+    }
+    if(!pages || pages < 0 || pages != Math.trunc(pages)){
+        // Количество страниц не мошен быть отрицательным или дробным
+        error = true;
+        error_pages.show();
+    }else{
+        error_pages.hide();
+    }
+    if(!date || date < 0 || date != Math.trunc(date) || date > current_date.getFullYear()){
+        // год издания не верен
+        error = true;
+        error_date.show();
+    }else{
+        error_date.hide();
+    }
+    return error;
+};
 
 let save_book = function(){
+    // сохраняем текущую редактируюмую книгу (создаем новую или редактируем старую)
+
     let name = $('#mkj_book_name').val();
     let author = $('#mkj_book_author').val();
     let pages = $('#mkj_book_pages').val();
     let date = $('#mkj_book_date').val();
-    //TODO: добавить валидацию
+
+    if(check_book_eroors()){
+        // ошибка, выходим без сохранения
+        return;
+    }
 
     if(current_book){
+        // книга есть, редактируем
         current_book.set_data(name, author, date, pages);
         for(let i=0; i<books_data.length; i++){
             if(books_data[i].id == current_book.id){
@@ -139,6 +197,7 @@ let save_book = function(){
         }
         return;
     }
+    // книги нет, создаем новую
     current_index++;
     let book = new Book(current_index, name, author, date, pages);
     books.push(book);
@@ -147,6 +206,7 @@ let save_book = function(){
 };
 
 let add_book = function() {
+    // создание новой книги, если есть выбранныая книга
     current_book = undefined;
     save_book();
 };
@@ -158,6 +218,7 @@ let save_books_data = function () {
 
 $(document).ready(function () {
     if(localStorage.saved_books){
+        // достаем книги из хранилища при запуске
         let book;
         books_data = JSON.parse(localStorage.saved_books);
         for(let i=0; i<books_data.length; i++){
